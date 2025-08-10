72d673858759b0a1e26f4993f8d4a4b03179d3e8
pmff95 <48364439+pmff95@users.noreply.github.com>
2025-08-10 16:25:46 -0300

    Separar campos de endereço no usuário

--- src/main/java/com/example/demo/dto/UsuarioDTO.java
+++ src/main/java/com/example/demo/dto/UsuarioDTO.java
@@ -13,7 +13,11 @@ public class UsuarioDTO {
     private String telefone;
     private String telefoneSecundario;
     private String email;
-    private String enderecoCompleto;
+    private String numero;
+    private String cep;
+    private String logradouro;
+    private String uf;
+    private String cidade;
     private Perfil perfil;
 
     public Long getId() {
@@ -80,12 +84,44 @@ public class UsuarioDTO {
         this.email = email;
     }
 
-    public String getEnderecoCompleto() {
-        return enderecoCompleto;
+    public String getNumero() {
+        return numero;
     }
 
-    public void setEnderecoCompleto(String enderecoCompleto) {
-        this.enderecoCompleto = enderecoCompleto;
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
     }
 
     public Perfil getPerfil() {
