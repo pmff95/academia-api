66e3b4ec81ee410a2db94496d31feaf9f06bde53
pmff95 <48364439+pmff95@users.noreply.github.com>
2025-08-12 11:19:24 -0300

    Envia email apos confirmar cadastro

--- src/main/java/com/example/demo/service/AcademiaService.java
+++ src/main/java/com/example/demo/service/AcademiaService.java
@@ -8,12 +8,16 @@ import com.example.demo.repository.AcademiaRepository;
 import com.example.demo.domain.enums.Perfil;
 import com.example.demo.common.util.SenhaUtil;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Service
+@Slf4j
 public class AcademiaService {
     private final AcademiaRepository repository;
     private final AcademiaMapper mapper;
@@ -30,15 +34,29 @@ public class AcademiaService {
 
     @Transactional
     public String create(AcademiaDTO dto) {
-        Academia entity = mapper.toEntity(dto);
-        Usuario admin = entity.getAdmin();
-        String senha = SenhaUtil.gerarSenhaNumerica(6);
-        admin.setSenha(passwordEncoder.encode(senha));
-        admin.setPerfil(Perfil.ADMIN);
-        admin.setAcademia(entity);
-        repository.save(entity);
-        emailService.enviarSenha(admin.getEmail(), senha, entity.getUuid().toString());
-        return "Academia criada com sucesso";
+        try {
+            Academia entity = mapper.toEntity(dto);
+            Usuario admin = entity.getAdmin();
+            String senha = SenhaUtil.gerarSenhaNumerica(6);
+            admin.setSenha(passwordEncoder.encode(senha));
+            admin.setPerfil(Perfil.ADMIN);
+            admin.setAcademia(entity);
+            repository.save(entity);
+            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+                @Override
+                public void afterCommit() {
+                    try {
+                        emailService.enviarSenha(admin.getEmail(), senha, entity.getUuid().toString());
+                    } catch (Exception e) {
+                        log.error("Erro ao enviar email", e);
+                    }
+                }
+            });
+            return "Academia criada com sucesso";
+        } catch (Exception e) {
+            log.error("Erro ao criar academia", e);
+            return "Erro ao criar academia";
+        }
     }
 
     public Page<AcademiaDTO> findAll(Pageable pageable) {
