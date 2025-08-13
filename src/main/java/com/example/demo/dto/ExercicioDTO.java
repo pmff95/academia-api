1399bf4a7c10c580d22c410b54d0dbdb21d3b9e1
pmff95 <48364439+pmff95@users.noreply.github.com>
2025-08-13 08:17:17 -0300

    Add description and muscle to Exercicio

--- src/main/java/com/example/demo/dto/ExercicioDTO.java
+++ src/main/java/com/example/demo/dto/ExercicioDTO.java
@@ -1,10 +1,13 @@
 package com.example.demo.dto;
 
+import com.example.demo.entity.Musculo;
 import java.util.UUID;
 
 public class ExercicioDTO {
     private UUID uuid;
     private String nome;
+    private String descricao;
+    private Musculo musculo;
 
     public UUID getUuid() {
         return uuid;
@@ -20,5 +23,21 @@ public class ExercicioDTO {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Musculo getMusculo() {
+        return musculo;
+    }
+
+    public void setMusculo(Musculo musculo) {
+        this.musculo = musculo;
     }
 }
---
+++ src/main/java/com/example/demo/entity/Musculo.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+public enum Musculo {
+    PEITO,
+    TRICEPS,
+    ABDOMEN,
+    QUADRICEPS,
+    OMBRO,
+    COSTAS,
+    PANTURRILHA
+}
