1e19a164cc637eb279d84ececf7d2a4d30cf97c1
pmff95 <48364439+pmff95@users.noreply.github.com>
2025-08-12 21:19:04 -0300

    Include user details in JWT token

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -3,13 +3,19 @@ package com.example.demo.dto;
 public class AuthResponse {
     private String token;
     private boolean primeiroAcesso;
+    private String nome;
+    private String email;
+    private String perfil;
 
     public AuthResponse() {
     }
 
-    public AuthResponse(String token, boolean primeiroAcesso) {
+    public AuthResponse(String token, boolean primeiroAcesso, String nome, String email, String perfil) {
         this.token = token;
         this.primeiroAcesso = primeiroAcesso;
+        this.nome = nome;
+        this.email = email;
+        this.perfil = perfil;
     }
 
     public String getToken() {
@@ -26,5 +32,29 @@ public class AuthResponse {
 
     public void setPrimeiroAcesso(boolean primeiroAcesso) {
         this.primeiroAcesso = primeiroAcesso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
     }
 }
