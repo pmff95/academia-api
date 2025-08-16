d48edd2f17193a77986a0efdd80e814b526da343
pmff95 <48364439+pmff95@users.noreply.github.com>
2025-08-15 21:34:01 -0300

    feat: add startup data insert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuario (uuid, nome, cpf, senha, perfil, ativo, tipo)
+VALUES ('00000000-0000-0000-0000-000000000001', 'Administrador', '00000000000', 'admin', 'ADMIN', true, 'Usuario');
